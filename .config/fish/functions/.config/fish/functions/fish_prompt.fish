5e2c410b589b938d4fa47688c634fd1fe890b8d8
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2022-08-21 19:33:37 +0000

    updated dotfiles

--- .config/fish/functions/.config/fish/functions/fish_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_prompt --description 'Write out the prompt'
-        if set -q GITHUB_USER
-            set FISH_USER $GITHUB_USER
-        else
-            set FISH_USER $USER
-        end
-        printf '%s%s@%s%s %s%s%s > ' (set_color green) $FISH_USER (set_color red) $hostname \
-            (set_color blue) (prompt_pwd) (set_color normal)
-end
