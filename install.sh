3ad0a89916def302b939aa2801b55b2e7d892b8b
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2022-09-27 18:08:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ if not test -d ~/.ssh
 end
 ssh-keyscan github.com >> "$HOME/.ssh/known_hosts"
 if set -q MY_SSH_KEY
-  echo "$MY_SSH_KEY" > "$HOME/.ssh/id_rsa"
+  echo "$MY_SSH_KEY" | base64 --decode "$HOME/.ssh/id_rsa"
   chmod 0600 "$HOME/.ssh/id_rsa"
 end
