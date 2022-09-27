890589f4cf3c9e8b3d6c5a665ebbbe1ddf35f740
Giridhar Salana <giridharsalana@gmail.com>
2022-09-27 17:08:23 +0530

    new

---
+++ bkp.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+ssh-keyscan github.com >> "$HOME/.ssh/known_hosts" \
+if set -q MY_SSH_KEY \
+  echo "$MY_SSH_KEY" > "$HOME/.ssh/id_rsa" \
+  chmod 0600 "${HOME}/.ssh/id_rsa" \
+end
