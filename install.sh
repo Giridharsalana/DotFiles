fb05ef34dfe2f384d8a85731564e88da4ac34c58
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2022-10-08 15:10:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ if set -q MY_SSH_KEY
   echo "$MY_SSH_KEY" | base64 --decode > "$HOME/.ssh/id_rsa"
   chmod 0600 "$HOME/.ssh/id_rsa"
 end
+
+if set -q CODESPACE_VSCODE_FOLDER
+  find /workspaces/.codespaces/.persistedshare/dotfiles/ -mindepth 1 -maxdepth 1 -not -path "*/.git/*" -not -path "*.git" -not -path "*/install.sh"  -print -exec cp -rsf {} ~/. \;
+end
