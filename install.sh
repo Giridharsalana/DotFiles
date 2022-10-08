0d761d7655115ef1efd80a9d1d1bc2b18cf168b3
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2022-10-08 15:38:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,7 @@ if set -q MY_SSH_KEY
   chmod 0600 "$HOME/.ssh/id_rsa"
 end
 
-echo "Came here"
 if set -q CODESPACES
-  echo "Came here too"
   find /workspaces/.codespaces/.persistedshare/dotfiles/ -mindepth 1 -maxdepth 1 -not -path "*/.git/*" -not -path "*.git" -not -path "*/install.sh" -not -path "*/README*" -print -exec cp -rsf {} ~/. \;
 end
 
