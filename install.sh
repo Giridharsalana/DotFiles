773675da9818e672ebb091f6d2525398e4f4dd52
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2023-09-16 23:52:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ if set -q MY_SSH_KEY
 end
 
 if set -q CODESPACES
+  test -f ~/.bashrc && cp ~/.bashrc ~/.bashrc_codespace_default_bkp
   find /workspaces/.codespaces/.persistedshare/dotfiles/ -mindepth 1 -maxdepth 1 -not -path "*/.git/*" -not -path "*.git" -not -path "*/install.sh" -not -path "*/README*" -print -exec cp -rf {} ~/. \;
 end
 
-if set -q GITPOD_USER
+if set -q GITPOD_GIT_USER_NAME
+  test -f ~/.bashrc && cp ~/.bashrc ~/.bashrc_gitpod_default_bkp
   find ~/.dotfiles/ -mindepth 1 -maxdepth 1 -not -path "*/.git/*" -not -path "*.git" -not -path "*/install.sh" -not -path "*/README*" -exec cp -rsf {} ~/. \;
 end
